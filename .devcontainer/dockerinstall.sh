af43ad20de8bad0cc7477d075685d7e0513e3cb8
Vlad Stirbu <vstirbu@gmail.com>
2024-03-21 10:51:34 +0200

    Update Docker and Kubernetes installation scripts

--- .devcontainer/dockerinstall.sh
+++ .devcontainer/dockerinstall.sh
@@ -1,3 +1,4 @@
+apt-get update -y
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
