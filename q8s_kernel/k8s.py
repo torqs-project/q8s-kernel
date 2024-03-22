f28139f469a962204d4a2ef11b0d157961ad093b
Vlad Stirbu <vstirbu@gmail.com>
2024-03-22 08:33:06 +0200

    Update logging format, add Docker image from environment

--- q8s_kernel/k8s.py
+++ q8s_kernel/k8s.py
@@ -9,9 +9,10 @@ import logging
 from python_on_whales import docker
 import kubernetes
 
-logging.basicConfig(level=logging.INFO)
+FORMAT = "[%(levelname)s %(asctime)-15s q8s_kernel] %(message)s"
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 
-TAG_TEMPLATE = Template('vstirbu/qiskit-aer-gpu:$version')
+TAG_TEMPLATE = Template("$image:$version")
 
 dockerfile_content = """
 FROM --platform=amd64 vstirbu/q8s-cuda12
@@ -48,27 +49,33 @@ entrypoint_content = """
 python3 ./main.py
 """
 
+
 def list_containers(client):
     print(client.containers.list())
 
+
 def create_temp_directory() -> str:
     temp_dir = tempfile.mkdtemp()
-    print("Temporary directory created:", temp_dir)
+    logging.debug("Temporary directory created:", temp_dir)
 
-    return temp_dir;
+    return temp_dir
+
 
 def delete_directory(directory_path):
     shutil.rmtree(directory_path)
     print("Directory deleted:", directory_path)
 
+
 def write_to_file(file_path: str, content: str):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         file.write(content)
     print("Content written to file:", file_path)
+
 
 def enable_executable(file_path: str):
     os.chmod(file_path, 0o755)
     print("Executable flag enabled for file:", file_path)
+
 
 def prepare_build_folder(temp_dir: str, python_file_content: str):
     write_to_file(temp_dir + "/Dockerfile", dockerfile_content)
@@ -77,6 +84,7 @@ def prepare_build_folder(temp_dir: str, python_file_content: str):
     write_to_file(temp_dir + "/main.py", python_file_content)
     enable_executable(temp_dir + "/entrypoint.sh")
 
+
 def print_nodes(api_instance):
     node_list = api_instance.list_node()
 
@@ -84,13 +92,14 @@ def print_nodes(api_instance):
     # Patching the node labels
     for node in node_list.items:
         # print("%s\t\t%s" % (node.metadata.name, node.metadata.labels))
-        print("%s\t\t%s" % ("NAME", node.metadata.name))   
+        print("%s\t\t%s" % ("NAME", node.metadata.name))
 
         print("%s\t\t%s" % ("LABEL", "VALUE"))
         for label, value in node.metadata.labels.items():
             print("%s\t\t%s" % (label, value))
 
-def create_job_object(name="qiskit-aer-gpu", image=TAG_TEMPLATE.substitute(version="latest")):
+
+def create_job_object(image, name="qiskit-aer-gpu"):
     # Configureate Pod template container
     container = kubernetes.client.V1Container(
         name=name,
@@ -98,39 +107,37 @@ def create_job_object(name="qiskit-aer-gpu", image=TAG_TEMPLATE.substitute(versi
         command=["python3"],
         args=["./main.py"],
         resources=kubernetes.client.V1ResourceRequirements(
-            limits={"nvidia.com/gpu": "1"},
-            requests={"nvidia.com/gpu": "1"}
-        )
+            limits={"nvidia.com/gpu": "1"}, requests={"nvidia.com/gpu": "1"}
+        ),
     )
 
     # Create and configurate a spec section
     template = kubernetes.client.V1PodTemplateSpec(
         metadata=kubernetes.client.V1ObjectMeta(labels={"app": name}),
-        spec=kubernetes.client.V1PodSpec(containers=[container], restart_policy="Never")
+        spec=kubernetes.client.V1PodSpec(
+            containers=[container], restart_policy="Never"
+        ),
     )
 
     # Create the specification of deployment
-    spec = kubernetes.client.V1JobSpec(
-        template=template
-    )
+    spec = kubernetes.client.V1JobSpec(template=template)
 
     # Instantiate the job object
     job = kubernetes.client.V1Job(
         api_version="batch/v1",
         kind="Job",
         metadata=kubernetes.client.V1ObjectMeta(name=name),
-        spec=spec
+        spec=spec,
     )
 
     return job
 
+
 def create_job(job):
     api_instance = kubernetes.client.BatchV1Api()
-    api_response = api_instance.create_namespaced_job(
-        body=job,
-        namespace="default"
-    )
+    api_response = api_instance.create_namespaced_job(body=job, namespace="default")
     logging.info("Job created. status='%s'" % str(api_response.status))
+
 
 def complete_and_get_job_status(name="qiskit-aer-gpu"):
     job_completed = False
@@ -139,32 +146,47 @@ def complete_and_get_job_status(name="qiskit-aer-gpu"):
     while not job_completed:
         api_response = api_instance.read_namespaced_job_status(name, "default")
 
-        if api_response.status.succeeded is not None or api_response.status.failed is not None:
+        if (
+            api_response.status.succeeded is not None
+            or api_response.status.failed is not None
+        ):
             job_completed = True
 
         sleep(1)
         logging.info("Job status='%s'" % str(api_response.status))
-    
+
     return api_response.status
+
 
 def get_pods_in_job(name="qiskit-aer-gpu"):
     api_instance = kubernetes.client.CoreV1Api()
-    api_response = api_instance.list_namespaced_pod("default", label_selector="app=qiskit-aer-gpu")
+    api_response = api_instance.list_namespaced_pod(
+        "default", label_selector="app=qiskit-aer-gpu"
+    )
     logging.info("Pods in the job='%s'" % str(api_response.items[0].metadata.name))
 
     return api_response.items[0].metadata.name
 
+
 def get_job_logs(name="qiskit-aer-gpu"):
-    api_response = kubernetes.client.CoreV1Api().read_namespaced_pod_log(name=name, namespace="default")
+    api_response = kubernetes.client.CoreV1Api().read_namespaced_pod_log(
+        name=name, namespace="default"
+    )
     logging.debug("Job logs='%s'" % str(api_response))
     return api_response
 
+
 def delete_job(name="qiskit-aer-gpu"):
-    stream = 'stdout'
-    api_response = kubernetes.client.BatchV1Api().delete_namespaced_job(name, "default", body=kubernetes.client.V1DeleteOptions(propagation_policy="Foreground"))
+    stream = "stdout"
+    api_response = kubernetes.client.BatchV1Api().delete_namespaced_job(
+        name,
+        "default",
+        body=kubernetes.client.V1DeleteOptions(propagation_policy="Foreground"),
+    )
     logging.info("Job deleted. status='%s'" % str(api_response.status))
 
     return api_response
+
 
 def map_job_status_to_stream(status):
     if status.succeeded is not None:
@@ -174,7 +196,8 @@ def map_job_status_to_stream(status):
     else:
         return "unknown"
 
-def execute(python_file_content: str) -> str:
+
+def execute(code: str, temp_dir: str, docker_image: str) -> str:
     # docker_client = docker.APIClient(base_url='unix://var/run/docker.sock')
     kubernetes.config.load_kube_config()
 
@@ -184,16 +207,15 @@ def execute(python_file_content: str) -> str:
 
     # print_nodes(api_instance)
 
-    if True:
-        temp_dir = "/var/folders/8n/b8xghht90n19tx0nqqx9qlk80000gp/T/tmp8hytthj6"
-    else:
-        temp_dir = create_temp_directory()
+    tag = TAG_TEMPLATE.substitute(image=docker_image, version=uuid.uuid4())
 
-    tag = TAG_TEMPLATE.substitute(version=uuid.uuid4())
+    prepare_build_folder(temp_dir, code)
+    image = docker.buildx.build(
+        context_path=temp_dir,
+        tags=[tag],
+        labels={"qubernetes.cloud/mode": "development"},
+    )
 
-    prepare_build_folder(temp_dir, python_file_content)
-    image = docker.buildx.build(context_path=temp_dir, tags=[tag], labels={"qubernetes.cloud/mode": "development"})
-    
     logging.info("built new image: %s" % image)
 
     docker.image.push(tag)
--- q8s_kernel/kernel.py
+++ q8s_kernel/kernel.py
@@ -1,3 +1,4 @@
+import os
 from time import time
 from ipykernel.kernelbase import Kernel
 import logging
@@ -6,56 +7,76 @@ from .k8s import execute
 
 USE_KUBERNETES = True
 
+
 class Q8sKernel(Kernel):
-    implementation = 'q8s-kernel'
-    implementation_version = '0.'
-    language = 'no-op'
-    language_version = '0.1'
+    implementation = "q8s-kernel"
+    implementation_version = "0."
+    language = "no-op"
+    language_version = "0.1"
     language_info = {
-        'name': 'Any text',
-        'mimetype': 'text/plain',
-        'file_extension': '.txt',
+        "name": "Any text",
+        "mimetype": "text/plain",
+        "file_extension": ".txt",
     }
     banner = "q8s"
 
-    def do_execute(self, code, silent, store_history=True, user_expressions=None,
-                   allow_stdin=False):
-        
-        if USE_KUBERNETES:
-            start = time()
-            output, stream_name = execute(code)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if True:
+            self.temp_dir = (
+                "/var/folders/8n/b8xghht90n19tx0nqqx9qlk80000gp/T/tmp8hytthj6"
+            )
+        else:
+            sefl.temp_dir = create_temp_directory()
 
-            stream_content = {'name': stream_name, 'text': output + f"\nExecution time: {time() - start:.2f} seconds"}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+    def do_execute(
+        self, code, silent, store_history=True, user_expressions=None, allow_stdin=False
+    ):
+
+        if USE_KUBERNETES:
+
+            start = time()
+            output, stream_name = execute(
+                code, self.temp_dir, os.environ["DOCKER_IMAGE"]
+            )
+
+            stream_content = {
+                "name": stream_name,
+                "text": output + f"\nExecution time: {time() - start:.2f} seconds",
+            }
+            self.send_response(self.iopub_socket, "stream", stream_content)
         else:
             if not silent:
                 # Write the code to a new Python file
-                with open('temp.py', 'w') as f:
+                with open("temp.py", "w") as f:
                     f.write(code)
 
             # Use subprocess to execute the file and capture the output and possible errors
-            result = subprocess.run(['python', 'temp.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output = result.stdout.decode('utf-8')
-            errors = result.stderr.decode('utf-8')
+            result = subprocess.run(
+                ["python", "temp.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            output = result.stdout.decode("utf-8")
+            errors = result.stderr.decode("utf-8")
 
-            stream_content = {'name': 'stdout', 'text': output}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+            stream_content = {"name": "stdout", "text": output}
+            self.send_response(self.iopub_socket, "stream", stream_content)
 
             if errors:
-                error_content = {'name': 'stderr', 'text': errors}
-                self.send_response(self.iopub_socket, 'stream', error_content)
+                error_content = {"name": "stderr", "text": errors}
+                self.send_response(self.iopub_socket, "stream", error_content)
 
             # Log the output and errors to the console and write them to a file
             logging.info(output)
             logging.error(errors)
-            with open('output.txt', 'a') as f:
+            with open("output.txt", "a") as f:
                 print(output, file=f)
                 if errors:
                     print(errors, file=f)
 
-        return {'status': 'ok',
-                # The base class increments the execution count
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {},
-               }
+        return {
+            "status": "ok",
+            # The base class increments the execution count
+            "execution_count": self.execution_count,
+            "payload": [],
+            "user_expressions": {},
+        }
